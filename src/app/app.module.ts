3c4ab8ed1ab59be8c57788d937c2f55d12e2e7b7
marwabahrini <marwabahrini08@gmail.com>
2024-02-24 00:14:46 +0100

    fix servers

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms'
+import {serverComponent} from "./server/server.component";
+import { AssignmentComponent } from './assignment/assignment.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    serverComponent,
+    AssignmentComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
